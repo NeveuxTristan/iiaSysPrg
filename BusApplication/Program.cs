04cf884e00bc0cd4231771af7e2987c58f2a4b09
Tristan <neveuxtristan@gmail.com>
2018-05-15 16:16:22 +0200

    Add skeleton classes.

--- BusApplication/Program.cs
+++ BusApplication/Program.cs
@@ -2,8 +2,16 @@
 {
     internal class Program
     {
+        
+        
+        
+        
+        
         public static void Main(string[] args)
         {
         }
+        
+        
+        
     }
 }
