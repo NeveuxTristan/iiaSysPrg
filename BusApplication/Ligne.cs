348e91c93c45c3956328e8680b1c08bc1d372a9a
Tristan <neveuxtristan@gmail.com>
2018-05-23 14:29:37 +0200

    Intelligence lignes et stations

--- BusApplication/Ligne.cs
+++ BusApplication/Ligne.cs
@@ -1,7 +1,39 @@
-﻿namespace BusApplication
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace BusApplication
 {
     public class Ligne
     {
+        private List<Station> stations;
+        private int nbStations;
+
+        public Ligne(int nbStations)
+        {
+            this.nbStations = nbStations;
+            stations = new List<Station>();
+            Thread th;
+            Station s;
+            for (int i = 0; i < nbStations; i++)
+            {
+                s = new Station();
+                th = new Thread(s.vieStation);
+                stations.Add(s);
+                th.Start();
+            }
+        }
+
+        public Station getStation(int id)
+        {
+            foreach (Station s in stations)
+            {
+                if (s.getId() == id)
+                {
+                    return s;
+                }
+            }
+            return null;
+        }
         
     }
 }
--- BusApplication/Station.cs
+++ BusApplication/Station.cs
@@ -1,7 +1,54 @@
-﻿namespace BusApplication
+﻿using System;
+using System.Threading;
+
+namespace BusApplication
 {
     public class Station
     {
+        private static int ID_REFERENCE = 1;
+
+        private Boolean isRunning = true; 
+        private int id;
+        private int nbClientAttente;
+        private Random r = new Random();
+
+        public Station()
+        {
+            id = ID_REFERENCE++;
+            nbClientAttente = Math.Max(r.Next(10), r.Next(5));
+        }
+
+        public void refillAttente()
+        {
+            nbClientAttente = Math.Max(r.Next(10), r.Next(5));
+        }
+
+        public void vieStation()
+        {
+            while (isRunning)
+            {
+                Thread.Sleep(3000);
+                if (r.Next(1000) < r.Next(500))
+                {
+                    nbClientAttente++;
+                }
+            }
+        }
+
+        public void shutdown()
+        {
+            isRunning = false;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public int getNbClient()
+        {
+            return nbClientAttente;
+        }
         
     }
 }
